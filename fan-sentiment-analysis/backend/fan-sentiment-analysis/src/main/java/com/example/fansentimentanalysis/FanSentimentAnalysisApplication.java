ed2ef55a483795df5263ecbd16d17eb6e4407756
cooptothe <edward.cooper97@gmail.com>
2024-07-01 17:39:11 -0500

    set backend

---
+++ fan-sentiment-analysis/backend/fan-sentiment-analysis/src/main/java/com/example/fansentimentanalysis/FanSentimentAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.fansentimentanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanSentimentAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanSentimentAnalysisApplication.class, args);
+	}
+
+}
---
+++ fan-sentiment-analysis/backend/fan-sentiment-analysis/src/test/java/com/example/fansentimentanalysis/FanSentimentAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fansentimentanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanSentimentAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

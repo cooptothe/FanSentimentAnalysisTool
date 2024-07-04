f3139e960ac1fe9b6122d1e9471cbf0d9060f469
cooptothe <edward.cooper97@gmail.com>
2024-07-03 20:37:12 -0500

    configuring backend

--- fan-sentiment-analysis/backend/fan-sentiment-analysis/src/main/java/com/example/fansentimentanalysis/controller/UserController.java
+++ fan-sentiment-analysis/backend/fan-sentiment-analysis/src/main/java/com/example/fansentimentanalysis/controller/UserController.java
@@ -1,10 +1,16 @@
 // UserController.java
 package com.example.fansentimentanalysis.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.fansentimentanalysis.model.User;
 import com.example.fansentimentanalysis.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
